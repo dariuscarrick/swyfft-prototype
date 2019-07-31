7c7a92b922b3c7d183f5232decd2908a58da26b8
DARIUS-LAPTOP\Darius <darius@swyfft.com>
2019-07-30 18:30:37 -0600

    Updates

---
+++ precache-manifest.467248ff58bfc77c1255989135356bdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec77835cd1e889ec052e878b60cb768",
+    "url": "/dariuscarrick/swyfft-prototype/index.html"
+  },
+  {
+    "revision": "13651e50b245e565e33b",
+    "url": "/dariuscarrick/swyfft-prototype/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/dariuscarrick/swyfft-prototype/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "13651e50b245e565e33b",
+    "url": "/dariuscarrick/swyfft-prototype/static/js/main.5dbe8ab5.chunk.js"
+  },
+  {
+    "revision": "bb244aece41b350c7e7d",
+    "url": "/dariuscarrick/swyfft-prototype/static/js/runtime~main.bb6cbf72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dariuscarrick/swyfft-prototype/static/media/logo.5d5d9eef.svg"
+  }
+]);
