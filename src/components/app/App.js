b7808c9bf00dc4f551696de9f977598d5e43f80a
DARIUS-LAPTOP\Darius <darius@swyfft.com>
2019-08-20 15:47:53 -0600

    Finish routing examples for navbar design

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,16 +3,22 @@ import {Route} from 'react-router-dom';
 import './App.scss';
 import NavBar from '../navbar/navbar';
 import Homeowners from '../homeowners/homeowners';
+import HomeownersQuote from '../quote/homeowners-quote';
 import ESCommercial from '../commercial/es-commercial/escommercial';
+import ESCommercialQuote from '../quote/es-commercial-quote';
 import AdmittedCommercial from '../commercial/admitted-commercial/admitted-commercial';
+import AdmittedCommercialQuote from '../quote/admitted-commercial-quote';
 
 function App() {
   return (
     <div>
-      <NavBar navbarClass='' />
-      <Route path='/' component={Homeowners} exact={true}/>
+      <NavBar />
+      <Route path='/Homeowners' component={Homeowners} exact={true}/>
+      <Route path='/Homeowners/Quote' component={HomeownersQuote} exact={true} />
       <Route path='/E&S-Commercial-Package' component={ESCommercial} exact={true}/>
+      <Route path='/E&S-Commercial-Package/Quote' component={ESCommercialQuote} exact={true}/>
       <Route path='/Commercial-Package' component={AdmittedCommercial} exact={true} />
+      <Route path='/Commercial-Package/Quote' component={AdmittedCommercialQuote} exact={true} />
     </div>
   );
 }
