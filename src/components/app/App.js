37808a2a134674da158a3ea926f5512b5f0c688d
DARIUS-LAPTOP\Darius <darius@swyfft.com>
2019-10-02 00:57:26 -0600

    Careers page work

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Route, Redirect, withRouter} from 'react-router-dom';
+import ScrollToTop from '../../components/component-assets/scrolling/scroll-top-top';
 import './App.scss';
 import NavBar from '../navbar/navbar';
 import Footer from '../footer/footer';
@@ -17,16 +18,18 @@ class App extends React.Component {
     const path = this.props.location.pathname;
     return (
       <div>
-        <Redirect exact from='/' to={path === '/' ? '/Homeowners' : path} />
-        <NavBar />
-        <Route path='/Homeowners' component={Homeowners} exact={true}/>
-        <Route path='/Homeowners/Quote' component={HomeownersQuote} exact={true} />
-        <Route path='/E&S-Commercial-Package' component={ESCommercial} exact={true}/>
-        <Route path='/E&S-Commercial-Package/Quote' component={ESCommercialQuote} exact={true}/>
-        <Route path='/Commercial-Package' component={AdmittedCommercial} exact={true} />
-        <Route path='/Commercial-Package/Quote' component={AdmittedCommercialQuote} exact={true} />
-        <Route path='/Careers' component={Careers} exact={true} />
-        <Footer />
+        <ScrollToTop>
+          <Redirect exact from='/' to={path === '/' ? '/Homeowners' : path} />
+          <NavBar />
+          <Route path='/Homeowners' component={Homeowners} exact={true}/>
+          <Route path='/Homeowners/Quote' component={HomeownersQuote} exact={true} />
+          <Route path='/E&S-Commercial-Package' component={ESCommercial} exact={true}/>
+          <Route path='/E&S-Commercial-Package/Quote' component={ESCommercialQuote} exact={true}/>
+          <Route path='/Commercial-Package' component={AdmittedCommercial} exact={true} />
+          <Route path='/Commercial-Package/Quote' component={AdmittedCommercialQuote} exact={true} />
+          <Route path='/Careers' component={Careers} exact={true} />
+          <Footer />
+        </ScrollToTop>
     </div>
     );
   }
--- src/components/careers/careers.js
+++ src/components/careers/careers.js
@@ -1,11 +1,87 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import './careers.scss';
+import {ReactComponent as ShieldLogo} from '../../assets/images/shield.svg';
+import {ReactComponent as CareersIllustration} from '../../assets/images/careers-illustration.svg';
 
 export default class Careers extends React.Component {
+
     render() {
         return (
-            <div>Careers</div>
+            <main>
+                <section id='swyfft-come-work-with-us'>
+                    <Container fluid={true} className='careers-hero'>
+                        <Row>
+                            <Col sm={{span: 10, offset: 1}} lg={{span: 8, offset: 2}}>
+                                <ShieldLogo />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col sm={{span: 10, offset: 1}} lg={{span: 8, offset: 2}}>
+                                <h1>Come Work With Us</h1>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col sm={{span: 10, offset: 1}} lg={{span: 6, offset: 3}}>
+                                <h3>As a group of self-motivated innovators, we are looking for team members who offer innovative technical knowledge and insurance industry experience to challenge the old way of doing insurance business.</h3>
+                            </Col>
+                        </Row>
+                        <CareersIllustration className='careers-illustration' alt='Careers at Swyfft Insurance' />
+                    </Container>
+                </section>
+                <section id='swyfft-job-listings'>
+                    <Container fluid={true} className='careers-job-listings'>
+                        <Row>
+                            <Col sm={{span: 10, offset: 1}} lg={{span: 8, offset: 2}}>
+                                <h2>Current Openings</h2>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col sm={{span: 10, offset: 1}} lg={{span: 8, offset: 2}}>
+                                <div class="BambooHR-ATS-board">
+                                    <h2>Open Positions</h2>
+                                    <ul class="BambooHR-ATS-Department-List">
+                                        <li id="bhrDepartmentID_18439" class="BambooHR-ATS-Department-Item">
+                                            <div id="department_18439"class="BambooHR-ATS-Department-Header">Customer Support</div>
+                                            <ul class="BambooHR-ATS-Jobs-List">
+                                                <li id="bhrPositionID_28" class="BambooHR-ATS-Jobs-Item">
+                                                    <a href="//swyfft.bamboohr.com/jobs/view.php?id=28">Customer Service Supervisor</a>
+                                                    <span class="BambooHR-ATS-Location">Jacksonville, Florida</span>
+                                                </li>
+                                                <li id="bhrPositionID_28" class="BambooHR-ATS-Jobs-Item">
+                                                    <a href="//swyfft.bamboohr.com/jobs/view.php?id=28">Lead Poetry Analyst</a>
+                                                    <span class="BambooHR-ATS-Location">Jacksonville, Florida</span>
+                                                </li>
+                                                <li id="bhrPositionID_28" class="BambooHR-ATS-Jobs-Item">
+                                                    <a href="//swyfft.bamboohr.com/jobs/view.php?id=28">Bilbo Baggins Look-alike</a>
+                                                    <span class="BambooHR-ATS-Location">Morristown, New Jersey</span>
+                                                </li>
+                                                <li id="bhrPositionID_28" class="BambooHR-ATS-Jobs-Item">
+                                                    <a href="//swyfft.bamboohr.com/jobs/view.php?id=28">Senior iOS Developer</a>
+                                                    <span class="BambooHR-ATS-Location">Redmond, WA</span>
+                                                </li>
+                                                <li id="bhrPositionID_28" class="BambooHR-ATS-Jobs-Item">
+                                                    <a href="//swyfft.bamboohr.com/jobs/view.php?id=28">Junior Software Developer</a>
+                                                    <span class="BambooHR-ATS-Location">Redmond, WA</span>
+                                                </li>
+                                                <li id="bhrPositionID_28" class="BambooHR-ATS-Jobs-Item">
+                                                    <a href="//swyfft.bamboohr.com/jobs/view.php?id=28">Claims Manager</a>
+                                                    <span class="BambooHR-ATS-Location">Morristown, NJ</span>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
+                <section id='swyfft-benefits'>
+                    <Container className='benefits' fluid={true}>
+
+                    </Container>
+                </section>
+            </main>
         )
     }
 }
