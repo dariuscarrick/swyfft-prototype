e2d19bd12e7142463c554402d283d6180717c630
DARIUS-LAPTOP\Darius <darius@swyfft.com>
2019-10-02 10:10:26 -0600

    Add open app section

--- src/components/careers/careers.js
+++ src/components/careers/careers.js
@@ -3,6 +3,7 @@ import {Container, Row, Col} from 'react-bootstrap';
 import './careers.scss';
 import {ReactComponent as ShieldLogo} from '../../assets/images/shield.svg';
 import {ReactComponent as CareersIllustration} from '../../assets/images/careers-illustration.svg';
+import {ReactComponent as Resume} from '../../assets/images/resume.svg';
 
 export default class Careers extends React.Component {
 
@@ -12,18 +13,19 @@ export default class Careers extends React.Component {
                 <section id='swyfft-come-work-with-us'>
                     <Container fluid={true} className='careers-hero'>
                         <Row>
-                            <Col sm={{span: 10, offset: 1}} lg={{span: 8, offset: 2}}>
+                            <Col sm={{span: 10, offset: 1}} lg={{span: 6, offset: 3}}>
                                 <ShieldLogo />
                             </Col>
                         </Row>
                         <Row>
-                            <Col sm={{span: 10, offset: 1}} lg={{span: 8, offset: 2}}>
+                            <Col sm={{span: 10, offset: 1}} lg={{span: 6, offset: 3}}>
+                                <h3>Let's Shake Things Up</h3>
                                 <h1>Come Work With Us</h1>
                             </Col>
                         </Row>
                         <Row>
                             <Col sm={{span: 10, offset: 1}} lg={{span: 6, offset: 3}}>
-                                <h3>As a group of self-motivated innovators, we are looking for team members who offer innovative technical knowledge and insurance industry experience to challenge the old way of doing insurance business.</h3>
+                                <h2>Through a mix of big data, human ingenuity and imagination that never stops, our team is transforming the way people experience insurance. We’re looking for like-minded, self-motivated innovators to join us as we challenge the old way of doing business.</h2>
                             </Col>
                         </Row>
                         <CareersIllustration className='careers-illustration' alt='Careers at Swyfft Insurance' />
@@ -76,9 +78,27 @@ export default class Careers extends React.Component {
                         </Row>
                     </Container>
                 </section>
+                <section id='open-application' className='careers-open-application'>
+                    <Container fluid={true}>
+                        <Row>
+                            <Col sm={{span: 10, offset: 1}} lg={{span: 8, offset: 2}} className='open-app-container'>
+                                <Resume />
+                                <aside class='open-app-actions'>
+                                    <h2>Don't See Your Role?</h2>
+                                    <p>Send us your resume and we’ll save it for when the perfect position comes up.</p>
+                                    <a className='apply-button'>Apply Now</a>
+                                </aside>
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
                 <section id='swyfft-benefits'>
                     <Container className='benefits' fluid={true}>
-
+                        <Row>
+                            <Col sm={{span: 10, offset: 1}} lg={{span: 8, offset: 2}}>
+                                <h2>Benefits & Perks</h2>
+                            </Col>
+                        </Row>
                     </Container>
                 </section>
             </main>
